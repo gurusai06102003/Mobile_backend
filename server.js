1291f1aad75e40650ca44d78012e71e7b7124fd9
gurusai06102003 <132398700+gurusai06102003@users.noreply.github.com>
2025-02-25 17:40:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "http://localhost:3000", 
+    origin: "https://mobilecomparision.vercel.app", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
